2055d05368ee87c6cd8947e1616c364869fbb3fd
Box <958396826@qq.com>
2021-08-07 12:36:27 +0800

    修改主站支持的最大从机数 从1增加到16个从机

--- FreeModbus/modbus/include/mbconfig.h
+++ FreeModbus/modbus/include/mbconfig.h
@@ -48,24 +48,24 @@ PR_BEGIN_EXTERN_C
  *  @{
  */
 /*! \brief If Modbus Master ASCII support is enabled. */
-#define MB_MASTER_ASCII_ENABLED                 (  0 )
+#define MB_MASTER_ASCII_ENABLED (0)
 /*! \brief If Modbus Master RTU support is enabled. */
-#define MB_MASTER_RTU_ENABLED                   (  1 )
+#define MB_MASTER_RTU_ENABLED (1)
 /*! \brief If Modbus Master TCP support is enabled. */
-#define MB_MASTER_TCP_ENABLED                   (  0 )
+#define MB_MASTER_TCP_ENABLED (0)
 /*! \brief If Modbus Slave ASCII support is enabled. */
-#define MB_SLAVE_ASCII_ENABLED                  (  0 )
+#define MB_SLAVE_ASCII_ENABLED (0)
 /*! \brief If Modbus Slave RTU support is enabled. */
-#define MB_SLAVE_RTU_ENABLED                    (  1 )
+#define MB_SLAVE_RTU_ENABLED (1)
 /*! \brief If Modbus Slave TCP support is enabled. */
-#define MB_SLAVE_TCP_ENABLED                    (  0 )
+#define MB_SLAVE_TCP_ENABLED (0)
 /*! \brief The character timeout value for Modbus ASCII.
  *
  * The character timeout value is not fixed for Modbus ASCII and is therefore
  * a configuration option. It should be set to the maximum expected delay
  * time of the network.
  */
-#define MB_ASCII_TIMEOUT_SEC                    (  1 )
+#define MB_ASCII_TIMEOUT_SEC (1)
 /*! \brief Maximum number of Modbus functions codes the protocol stack
  *    should support.
  *
@@ -73,7 +73,7 @@ PR_BEGIN_EXTERN_C
  * the sum of all enabled functions in this file and custom function
  * handlers. If set to small adding more functions will fail.
  */
-#define MB_FUNC_HANDLERS_MAX                    ( 16 )
+#define MB_FUNC_HANDLERS_MAX (16)
 /*! \brief Number of bytes which should be allocated for the <em>Report Slave ID
  *    </em>command.
  *
@@ -82,43 +82,43 @@ PR_BEGIN_EXTERN_C
  * how to set this value. It is only used if MB_FUNC_OTHER_REP_SLAVEID_ENABLED
  * is set to <code>1</code>.
  */
-#define MB_FUNC_OTHER_REP_SLAVEID_BUF           ( 32 )
+#define MB_FUNC_OTHER_REP_SLAVEID_BUF (32)
 /*! \brief If the <em>Report Slave ID</em> function should be enabled. */
-#define MB_FUNC_OTHER_REP_SLAVEID_ENABLED       (  1 )
+#define MB_FUNC_OTHER_REP_SLAVEID_ENABLED (1)
 /*! \brief If the <em>Read Input Registers</em> function should be enabled. */
-#define MB_FUNC_READ_INPUT_ENABLED              (  1 )
+#define MB_FUNC_READ_INPUT_ENABLED (1)
 /*! \brief If the <em>Read Holding Registers</em> function should be enabled. */
-#define MB_FUNC_READ_HOLDING_ENABLED            (  1 )
+#define MB_FUNC_READ_HOLDING_ENABLED (1)
 /*! \brief If the <em>Write Single Register</em> function should be enabled. */
-#define MB_FUNC_WRITE_HOLDING_ENABLED           (  1 )
+#define MB_FUNC_WRITE_HOLDING_ENABLED (1)
 /*! \brief If the <em>Write Multiple registers</em> function should be enabled. */
-#define MB_FUNC_WRITE_MULTIPLE_HOLDING_ENABLED  (  1 )
+#define MB_FUNC_WRITE_MULTIPLE_HOLDING_ENABLED (1)
 /*! \brief If the <em>Read Coils</em> function should be enabled. */
-#define MB_FUNC_READ_COILS_ENABLED              (  1 )
+#define MB_FUNC_READ_COILS_ENABLED (1)
 /*! \brief If the <em>Write Coils</em> function should be enabled. */
-#define MB_FUNC_WRITE_COIL_ENABLED              (  1 )
+#define MB_FUNC_WRITE_COIL_ENABLED (1)
 /*! \brief If the <em>Write Multiple Coils</em> function should be enabled. */
-#define MB_FUNC_WRITE_MULTIPLE_COILS_ENABLED    (  1 )
+#define MB_FUNC_WRITE_MULTIPLE_COILS_ENABLED (1)
 /*! \brief If the <em>Read Discrete Inputs</em> function should be enabled. */
-#define MB_FUNC_READ_DISCRETE_INPUTS_ENABLED    (  1 )
+#define MB_FUNC_READ_DISCRETE_INPUTS_ENABLED (1)
 /*! \brief If the <em>Read/Write Multiple Registers</em> function should be enabled. */
-#define MB_FUNC_READWRITE_HOLDING_ENABLED       (  1 )
+#define MB_FUNC_READWRITE_HOLDING_ENABLED (1)
 /*! @} */
 #ifdef __cplusplus
-    PR_END_EXTERN_C
+PR_END_EXTERN_C
 #endif
 
 #if MB_MASTER_RTU_ENABLED > 0 || MB_MASTER_ASCII_ENABLED > 0
 /*! \brief If master send a broadcast frame,the master will wait time of convert to delay,
  * then master can send other frame */
-  #define MB_MASTER_DELAY_MS_CONVERT              (200 )
+#define MB_MASTER_DELAY_MS_CONVERT (200)
 /*! \brief If master send a frame which is not broadcast,the master will wait sometime for slave.
  * And if slave is not respond in this time,the master will process this timeout error.
  * Then master can send other frame */
-#define MB_MASTER_TIMEOUT_MS_RESPOND            (100 )
+#define MB_MASTER_TIMEOUT_MS_RESPOND (100)
 /*! \brief The total slaves in Modbus Master system. Default 16.
  * \note : The slave ID must be continuous from 1.*/
-#define MB_MASTER_TOTAL_SLAVE_NUM               ( 1 )
+#define MB_MASTER_TOTAL_SLAVE_NUM (16)
 #endif
 
 #endif
